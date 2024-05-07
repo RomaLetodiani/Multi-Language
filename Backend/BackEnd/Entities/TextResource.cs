bf487065b7dc9063ffc9ff03bb7e3b4d6f1b9fb5
RomaLetodiani <roman.letodiani03@gmail.com>
2024-05-07 19:12:26 +0400

    Create TextResource.cs

---
+++ Backend/BackEnd/Entities/TextResource.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+
+namespace BackEnd.Entities
+{
+    public class TextResource
+    {
+        public int Id { get; set; }
+        public string Text { get; set; }
+
+        public int LanguageId { get; set; }
+
+        [ForeignKey("LanguageId")]  // Define foreign key relationship
+        public Language Language { get; set; }  // Navigation property
+    }
+}
