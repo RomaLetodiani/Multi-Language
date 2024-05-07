9b736a71f9a6ca43d8ded1072b58d074fed1ba00
RomaLetodiani <roman.letodiani03@gmail.com>
2024-05-08 03:22:54 +0400

    Index for import/export of Pages

---
+++ Frontend/src/pages/index.ts
@@ -0,0 +1,6 @@
+import About from './About/About'
+import Hello from './Hello/Hello'
+import Home from './Home/Home'
+import ErrorPage from './Error/ErrorPage'
+
+export { About, Hello, ErrorPage, Home }
