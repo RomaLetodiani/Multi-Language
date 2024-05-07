af149ba7cbf126895a82961c6274a66b505f03d0
RomaLetodiani <roman.letodiani03@gmail.com>
2024-05-08 03:22:36 +0400

    Index for import/export of hooks

---
+++ Frontend/src/hooks/index.ts
@@ -0,0 +1,5 @@
+import useMediaQuery from './useMediaQuery'
+import { useLocalStorage } from './useLocalStorage'
+import { useInput } from './useInput'
+
+export { useMediaQuery, useLocalStorage, useInput }
