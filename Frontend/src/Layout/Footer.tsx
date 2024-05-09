4c9f7ef05a0d431f9c4db2801b8c5e28d3c671f1
RomaLetodiani <roman.letodiani03@gmail.com>
2024-05-09 18:14:29 +0400

    update layout

--- Frontend/src/Layout/Footer.tsx
+++ Frontend/src/Layout/Footer.tsx
@@ -1,5 +1,14 @@
+import Text from '../components/Text'
+
 const Footer = () => {
-  return <div>Footer</div>
+  return (
+    <div className="p-5 bg-sky-800 text-white text-center">
+      <h3 className="font-bold text-lg mb-3">
+        <Text TextKey="FooterH3" />
+      </h3>
+      <p>© 2024</p>
+    </div>
+  )
 }
 
 export default Footer
--- Frontend/src/Layout/Header.tsx
+++ Frontend/src/Layout/Header.tsx
@@ -1,5 +1,30 @@
+import { Link } from 'react-router-dom'
+import Text from '../components/Text'
+import LanguageSwitcher from '../components/LanguageSwitcher'
+
 const Header = () => {
-  return <div>Header</div>
+  return (
+    <div className="p-5 flex justify-between items-center bg-sky-50 text-sky-700">
+      <Link to={'/'}>
+        <h1 className="text-3xl tracking-widest font-bold">MLW</h1>
+      </Link>
+      <nav className="flex items-center justify-center gap-10">
+        <LanguageSwitcher />
+        <ul className="flex gap-5">
+          <Link to={'about'}>
+            <li>
+              <Text TextKey="HeaderLi-1" />
+            </li>
+          </Link>
+          <Link to={'hello'}>
+            <li>
+              <Text TextKey="HeaderLi-2" />
+            </li>
+          </Link>
+        </ul>
+      </nav>
+    </div>
+  )
 }
 
 export default Header
