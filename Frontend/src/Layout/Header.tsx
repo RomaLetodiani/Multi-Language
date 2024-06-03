e299c7113e7ea8b859fbb9bd4ec154ffa3544f10
RomaLetodiani <roman.letodiani03@gmail.com>
2024-06-03 14:56:04 +0400

    logo

--- Frontend/src/Layout/Header.tsx
+++ Frontend/src/Layout/Header.tsx
@@ -1,12 +1,15 @@
 import { Link } from 'react-router-dom'
 import Text from '../components/Text'
 import LanguageSwitcher from '../components/LanguageSwitcher'
+import logo from '../Assets/Logo.png'
 
 const Header = () => {
   return (
     <div className="p-5 flex justify-between items-center bg-sky-50 text-sky-700">
       <Link to={'/'}>
-        <h1 className="text-3xl tracking-widest font-bold">MLW</h1>
+        <div className="w-[200px]">
+          <img src={logo} alt="MLW Logo" />
+        </div>
       </Link>
       <nav className="flex items-center justify-center gap-10">
         <LanguageSwitcher />
