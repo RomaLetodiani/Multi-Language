33ec9af3d8422f3f5f3c9a781b79a686056166a4
RomaLetodiani <roman.letodiani03@gmail.com>
2024-06-03 14:56:25 +0400

    Update DataContext.cs

--- Backend/BackEnd/Data/DataContext.cs
+++ Backend/BackEnd/Data/DataContext.cs
@@ -5,12 +5,37 @@ namespace BackEnd.Data
 {
     public class DataContext : DbContext
     {
-        public DataContext(DbContextOptions<DataContext> options): base(options)
+        public DataContext(DbContextOptions<DataContext> options)
+    : base(options)
         {
-            
         }
+
         public DbSet<Language> Languages { get; set; }
-        public DbSet<TextResource> TextResources { get; set; }
         public DbSet<Page> Pages { get; set; }
+        public DbSet<TextResource> TextResources { get; set; }
+        public DbSet<PageTextResource> PageTextResources { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+            modelBuilder.Entity<PageTextResource>()
+                .HasKey(pt => new { pt.PageId, pt.TextResourceId });
+
+            modelBuilder.Entity<PageTextResource>()
+                .HasOne(pt => pt.Page)
+                .WithMany(p => p.PageTextResource)
+                .HasForeignKey(pt => pt.PageId);
+
+            modelBuilder.Entity<PageTextResource>()
+                .HasOne(pt => pt.TextResource)
+                .WithMany(tr => tr.PageTextResource)
+                .HasForeignKey(pt => pt.TextResourceId);
+
+            modelBuilder.Entity<TextResource>()
+                .HasOne(tr => tr.Language)
+                .WithMany()
+                .HasForeignKey(tr => tr.LanguageId);
+        }
     }
 }
